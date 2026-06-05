234123b24e62b0d02a59cf5c7231ba513665b636
Lycolia Rizzim <33796432+Lycolia@users.noreply.github.com>
2026-06-05 17:31:28 +0900

    パス指定だけでなく日付指定などでも叩けるようにした

--- wk/timesheet.pl
+++ wk/timesheet.pl
@@ -5,6 +5,11 @@
 ### 日時ログを読み込み、TLセクションにある内容から、作業時間を割り出すスクリプト
 ###
 ### 使い方：
+###   ./timesheet.pl
+###   ./timesheet.pl 7
+###   ./timesheet.pl 27
+###   ./timesheet.pl 0527
+###   ./timesheet.pl 20260527
 ###   ./timesheet.pl log/2026/05/w5/27/log.md
 ###
 ### 入出力例：
@@ -33,11 +38,14 @@ use warnings;
 use utf8;
 use open qw(:std :encoding(UTF-8));
 use FindBin qw($Bin);
+use POSIX qw(strftime mktime);
+use Time::Piece;
 use lib "$Bin/lib";
 use TLParse;
 
-my $path = shift @ARGV
+my $argv = shift @ARGV
     or die "usage: $0 <log.md>\n";
+my $path = create_path($argv);
 
 my $entries = TLParse::parse_file($path);
 
@@ -53,4 +61,50 @@ for my $e (@$entries) {
 
 for my $name (@order) {
     printf "%s\t%g\n", $name, $hours{$name};
+}
+
+sub create_dir_path {
+    my $base_path = "${Bin}/log";
+    my $t = localtime;
+    my $year = shift // $t->year;
+    my $month = shift // $t->mon;
+    my $day = shift // $t->day;
+
+    my @now = localtime;
+
+    my $epoch_target_month_first_day = mktime(0, 0, 0, 1, $month - 1, $year - 1900);
+    my $epoch_target_day = mktime(0, 0, 0, $day, $month - 1, $year - 1900);
+
+    my $target_month_first_week = strftime('%W', localtime($epoch_target_month_first_day));
+    my $target_day_week  = strftime('%W', localtime($epoch_target_day));
+    my $week = $target_day_week - $target_month_first_week + 1;
+
+    return sprintf("%s/%04d/%02d/w%d/%02d", $base_path, $year, $month, $week, $day);
+}
+
+sub create_path {
+    my $param = shift;
+    my $base_path = 'log';
+
+    unless (defined $param) {
+        my $path = create_dir_path(undef, undef, undef);
+        return "${path}/log.md";
+    }
+
+    if ($param =~ /^(\d{1,2})$/) {
+        my $path = create_dir_path(undef, undef, $1);
+        return "${path}/log.md";
+    } elsif ($param =~ /^(\d{2}).*(\d{2})$/) {
+        my $path = create_dir_path(undef, $1, $2);
+        return "${path}/log.md";
+    } elsif ($param =~ /^(\d{4}).*(\d{2}).*(\d{2})$/) {
+        my $path = create_dir_path($1, $2, $3);
+        return "${path}/log.md";
+    } else {
+        if (-f $param) {
+            return $param;
+        } else {
+            die '引数がおかしい\n';
+        }
+    }
 }
